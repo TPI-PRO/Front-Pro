183ce626be1fcc0526fe362fc4134c851c8cf1b1
anferoar96 <afromana@unal.edu.co>
2019-11-17 12:40:41 -0500

    First view

---
+++ src/components/Image/Image.js
@@ -0,0 +1,19 @@
+import React from 'react'; 
+import Imagen from '../../assets/earth.svg';
+import Logo from '../../assets/logo1.svg'
+import './Image.css'
+import Aux from '../../hoc/Aux';
+
+const  Image=(props)=>(
+    
+    <Aux>
+        <img src={Logo} alt="Aqui no no hay nada" className="center"/>
+        <div className="container">
+      
+        <img src={Imagen} alt="Aqui no no hay nada" className="earth"/>
+        </div>
+    </Aux>
+    
+); 
+
+export default  Image;
---
+++ src/containers/LoginView/LoginView.js
@@ -0,0 +1,22 @@
+import React,{Component}   from 'react'; 
+import Image from '../../components/Image/Image';
+import './LoginView.css'
+
+class LoginView extends Component{
+
+    state={
+
+    }
+
+    render(){
+        return(
+            <div className="back">
+                <Image>
+                    
+                </Image>
+            </div>
+        );
+    }
+}
+
+export default LoginView ;
