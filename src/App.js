86282b8428607c6b2286f43b2f0f37a541d335af
anferoar96 <afromana@unal.edu.co>
2019-11-17 20:48:25 -0500

    SliderView

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import LoginView from './containers/LoginView/LoginView';
 import { BrowserRouter, Route,Switch} from "react-router-dom";
+import SliderView from './containers/SliderView/SliderView';
 
 function App() {
   return (
     <BrowserRouter>
         <Switch>
           <Route exact path="/" component={LoginView} /> {/*Esta es announcements*/}
-          {/*<Route path="/camunda" component={TestBPMN} />*/}
+          <Route path="/slide" component={SliderView} />
 
         </Switch>
     
