c6f4ba41e37d5927630a5b2d2eff5d07584fad1d
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2019-11-22 21:23:14 -0500

    changes in slide

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,26 +1,55 @@
 import React from 'react'; 
-import earth from '../../assets/earth2.svg';
+
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import Aux from '../../hoc/Aux';
 import consumo from '../../assets/consumo.svg';
 import consumismo from '../../assets/consumismo.svg';
-import responsable from '../../assets/responsable.svg';
+import earth from '../../assets/earth2.svg';
+import Fab from '@material-ui/core/Fab';
 import falsos from '../../assets/falsos.svg';
+import Grid from '@material-ui/core/Grid';
+
 import postconsumo from '../../assets/postconsumo.svg';
+import responsable from '../../assets/responsable.svg';
 import './Slider.css'
-import Aux from '../../hoc/Aux';
+
 
 const  Image=(props)=>(
-    
    <Aux>
-      <div className="slider">
-         <ul>
-            <li><img src={earth} alt="Aqui no no hay nada"  className="image"/></li>
-            <li><img src={consumo} alt="Aqui no no hay nada" className="image"/></li>
-            <li><img src={consumismo} alt="Aqui no no hay nada" className="image"/></li>
-            <li><img src={responsable} alt="Aqui no no hay nada" className="image"/></li>
-            <li><img src={falsos} alt="Aqui no no hay nada" className="image"/></li>
-            <li><img src={postconsumo} alt="Aqui no no hay nada" className="image"/></li>
-         </ul>
-      </div>
+      <Grid container direction="row">
+         <Grid item md={2} sm={2}>
+            <div className="navbar">
+               hallo
+            </div>
+         </Grid>
+         <Grid item md={1} sm={1} className="arrow">
+            <Fab color="secondary" size={'small'}>
+               <ArrowBackIosIcon/>
+            </Fab>
+         
+         </Grid>
+         <Grid item md={8} sm={8}>
+            <div className="slider">
+               <ul>
+                  <li><img src={earth} alt="Aqui no no hay nada"  className="image"/></li>
+                  <li><img src={consumo} alt="Aqui no no hay nada" className="image"/></li>
+                  <li><img src={consumismo} alt="Aqui no no hay nada" className="image"/></li>
+                  <li><img src={responsable} alt="Aqui no no hay nada" className="image"/></li>
+                  <li><img src={falsos} alt="Aqui no no hay nada" className="image"/></li>
+                  <li><img src={postconsumo} alt="Aqui no no hay nada" className="image"/></li>
+               </ul>
+            </div>
+            
+         </Grid>
+         <Grid item md={1} sm={1} className="arrow">
+            <Fab color="secondary" size={'small'}>
+               <ArrowForwardIosIcon/>
+            </Fab>   
+            
+         </Grid>
+      </Grid>
+      
       
    </Aux>
     
